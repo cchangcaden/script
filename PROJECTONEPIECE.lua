1c1ebef99aaf3826ab2e29b4cc0f9d29095219f8
AltsegoD <59056606+AltsegoD@users.noreply.github.com>
2020-12-08 11:24:28 +0700

    Create PROJECTONEPIECE.lua

---
+++ PROJECTONEPIECE.lua
@@ -0,0 +1,5 @@
+  game.StarterGui:SetCore("SendNotification", {
+  Title = "WARNING!";
+  Text = "The game updated, they added new AntiCheat, working in fixing, check back later.";
+  Duration = 5;
+  })
