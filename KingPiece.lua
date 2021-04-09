a11cc54c3577174db5fad4d4cc7ddabf7aa46e7f
AltsegoD <59056606+AltsegoD@users.noreply.github.com>
2021-04-09 19:48:02 +0700

    Update KingPiece.lua

--- KingPiece.lua
+++ KingPiece.lua
@@ -1,5 +1,5 @@
 game.StarterGui:SetCore("SendNotification", {
     Title = "egoD's King Piece Script";
-    Text = "Script closed, you can get banned for using this";
+    Text = "Script closed, you can get banned for using this script";
     Duration = 15;
 })
